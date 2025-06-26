cc4b28003aa5001647a80f34f183849d99187634
Maria Konova <Accercle@yandex.ru>
2025-06-26 23:19:51 +0300

    Реализация компонента OverlayMenu

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -29,29 +29,34 @@ const Header = (props) => {
     ] 
 
     return (
-    <header className='header'>
+    <header className='header' data-js-overlay-menu="">
         <div className="header__inner container">
             <Logo 
                 className="header__logo"
                 loading="eager" 
             />
-            <nav className="header__menu">
-                <ul className="header__menu-list">
-                    {menuItems.map(({ label, href }, index) => (
-                        <li className="header__menu-item" key={index}>
-                            <a 
-                                href={href} 
-                                className={classNames('header__menu-link', {
-                                    'is-active': href === url
-                                })}
-                            >
-                                {label}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-            <div className="header__actions">
+
+            <dialog 
+                className="header__overlay-menu-dialog" 
+                data-js-overlay-menu-dialog=""
+            >
+                <nav className="header__menu">
+                    <ul className="header__menu-list">
+                        {menuItems.map(({ label, href }, index) => (
+                            <li className="header__menu-item" key={index}>
+                                <a 
+                                    href={href} 
+                                    className={classNames('header__menu-link', {
+                                        'is-active': href === url
+                                    })}
+                                >
+                                    {label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className="header__actions">
                 <Button 
                     className="header__button"
                     label="Search"
@@ -66,9 +71,14 @@ const Header = (props) => {
                     mode="transparent"
                     iconName="notification"
                 />
-            </div>
+                </div>
+            </dialog>
+
             <BurgerButton
-                className="header__burger-button"
+                className="header__burger-button visible-tablet"
+                extraAttrs={{
+                    'data-js-overlay-menu-burger-button': '',
+                }}
             >
 
             </BurgerButton>
